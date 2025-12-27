8ed8a09ed79a1711a30a0752d62b531f2f817417
Anuj-sapkota <anujsapkota808@gmail.com>
2025-12-27 11:59:10 +0545

    role based access implemented

---
+++ backend/src/middlewares/authorize.ts
@@ -0,0 +1,18 @@
+import { ServiceError } from "../errors/ServiceError.js";
+import type { AuthRequest } from "../types/auth.js";
+import type { NextFunction, Response } from "express";
+export const authorize = (...allowedRoles: string[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (!user) {
+      return next(new ServiceError("Unauthorized", 401));
+    }
+
+    if (!allowedRoles.includes(user.role)) {
+      return next(new ServiceError("Forbidden: Insufficient permissions", 403));
+    }
+
+    next(); 
+  };
+};
