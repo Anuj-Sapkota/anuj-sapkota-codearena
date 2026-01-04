9c94344f9652e98fc910e68cb61a33674c09b383
Anuj-sapkota <anujsapkota808@gmail.com>
2026-01-04 17:20:39 +0545

    turnstile fully implimented

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -4,10 +4,11 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "CodeArena - Learn and Grow",
   description: "Problem Solving and Learning",
-  keywords: "coding challenges, competitive programming, online judge, algorithm practice, code submission, problem solving platform, real-time coding, programming contests, online compiler, user dashboard, progress tracking, leaderboard, Node.js, Next.js, React, Express.js, PostgreSQL, Tailwind CSS, responsive web app, code evaluation, challenge categories, programming education",
+  keywords:
+    "coding challenges, competitive programming, online judge, algorithm practice, code submission, problem solving platform, real-time coding, programming contests, online compiler, user dashboard, progress tracking, leaderboard, Node.js, Next.js, React, Express.js, PostgreSQL, Tailwind CSS, responsive web app, code evaluation, challenge categories, programming education",
   icons: {
-    icon: "/favicon.png"
-  }
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <body>
         {children}
+        <script
+          src="https://challenges.cloudflare.com/turnstile/v0/api.js?render=explicit"
+          defer
+        ></script>
       </body>
     </html>
   );
