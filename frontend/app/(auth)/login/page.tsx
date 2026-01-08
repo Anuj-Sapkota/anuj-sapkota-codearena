7a0862b8e9c110048f546837dbb4adf0074bfd8d
Anuj-sapkota <anujsapkota808@gmail.com>
2026-01-08 21:16:53 +0545

    feat: enhance login and registration forms with success and switch handlers

--- frontend/app/(auth)/login/page.tsx
+++ frontend/app/(auth)/login/page.tsx
@@ -1,10 +1,19 @@
 "use client";
+import { useRouter } from "next/navigation";
 import LoginForm from "../../components/auth/LoginForm";
 
 const LoginPage:React.FC = () => {
+ const router = useRouter()
+  //on success redirect to arena page
+  const handleSuccess = () => {
+    router.push("/explore")
+  }
 
+  const handleSwitch = () => {
+router.push("/register")
+  } 
   return (
-   <LoginForm />
+   <LoginForm  onSuccess={handleSuccess} onSwitch={handleSwitch}/>
   );
 };
 
